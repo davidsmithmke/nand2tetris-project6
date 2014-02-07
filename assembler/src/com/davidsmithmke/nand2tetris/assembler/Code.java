b344c5a98a200cdccd3e827ac27188738ec624ba
David Smith <dsmith@davidsmithmke.com>
2014-02-07 11:35:32 -0600

    Add Code framework
    Add empty framework for Code class.  The Code class decodes mnemonics
    and returns the corresponding bit patterns as strings.
    
    Add: Code.java

---
+++ assembler/src/com/davidsmithmke/nand2tetris/assembler/Code.java
@@ -0,0 +1,18 @@
+package com.davidsmithmke.nand2tetris.assembler;
+
+public class Code {
+	//  Returns the binary code of the dest mnemonic.
+	public String dest(String mnemonic) {
+		throw new RuntimeException();
+	}
+	
+	//  Returns the binary code of the comp mnemonic.
+	public String comp(String mnemonic) {
+		throw new RuntimeException();
+	}
+	
+	//  Returns the binary code of the jump mnemonic.
+	public String jump(String mnemonic) {
+		throw new RuntimeException();
+	}
+}
